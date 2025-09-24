23012929d9f71de65d0a1d6d858a0e02e66c5d2b
Arya Srivastava <asrivastava16@gojira.cs.usfca.edu>
2025-09-24 14:35:12 -0700

    Implementation works but we need to use makefs to create directory inside of fog os

--- user/find.c
+++ user/find.c
@@ -33,7 +33,7 @@ findit(char *path, char *expr)
   int fd;
   struct dirent de;
   struct stat st;
-
+  printf("Path: %s, expr: %s\n", path, expr);
   if((fd = open(path, O_RDONLY)) < 0){
     fprintf(2, "find: cannot open %s\n", path);
     return;
@@ -75,7 +75,6 @@ findit(char *path, char *expr)
   	    	printf("find: cannot stat %s\n", buf);
   	        continue;
   	    }
-  	    //printf("%d %d %d %d\n", fmtname(buf), st.type, st.ino, (int) st.size);
 		if(st.type == T_DIR){
 			findit(buf, expr);
 		} else if (match(expr,de.name)) {
@@ -83,75 +82,9 @@ findit(char *path, char *expr)
 		}
   	}
   	close(fd);
-  } /*else {
-  	printf("Error found while traversing dirs \n");
-  }*/
-  /*
-  switch(st.type){
-  case T_DEVICE:
-  case T_FILE:
-    printf("%d %d %d %d\n", fmtname(path), st.type, st.ino, (int) st.size);
-    break;
-
-  case T_DIR:
-    if(strlen(path) + 1 + DIRSIZ + 1 > sizeof buf){
-      printf("find: path too long\n");
-      break;
-    }
-    
-    - recursion would happen when we're searching within directories to find the file name given in expr
-    - stringcmp(TFILE == expr), else 
-    - base case would be the tree for all files
-    
-    strcpy(buf, path);
-    p = buf+strlen(buf);
-    *p++ = '/';
-
-    
-    while(read(fd, &de, sizeof(de)) == sizeof(de)){
-      if(de.inum == 0)
-        continue;
-      memmove(p, de.name, DIRSIZ);
-      p[DIRSIZ] = 0;
-      if(stat(buf, &st) < 0){
-        printf("find: cannot stat %s\n", buf);
-        continue;
-      }
-      printf("%d %d %d %d\n", fmtname(buf), st.type, st.ino, (int) st.size);
-    }
-    break;
-  }
-  close(fd); */
+  } 
 }
-/*
-void
-findit(char *path, char *expression)
-{
-  int n, m;
-  char *p, *q;
-  int fd = 0;
-  printf("Expression: %s\n", expression);
-  printf("Path: %s \n", path);
 
-  m = 0;
-  while((n = read(fd, buf+m, sizeof(buf)-m-1)) > 0){
-    m += n;
-    buf[m] = '\0';
-    p = buf;
-    while((q = strchr(p, '\n')) != 0){
-      *q = 0;
-      if(match(path, p)){
-        *q = '\n';
-        write(1, p, q+1 - p);
-      }
-      p = q+1;
-    }
-    if(m > 0){
-      m -= p - buf;
-      memmove(buf, p, m);
-    }
-  }
-}*/
 int
 main(int argc, char *argv[])
 {
@@ -160,35 +93,6 @@ main(int argc, char *argv[])
 	    exit(1);
 	  }
 	  findit(argv[1], argv[2]);
-/*
-  int fd, i;
-  char *path;
-  char *expression;
-
-  printf("in user space find.c file\n");
-
-  if(argc <= 1){
-    fprintf(2, "usage: find pattern [file ...]\n");
-    exit(1);
-  }
-  path = argv[1];
-  expression = argv[2];
-
-  if(argc <= 2){
-    findit(path, expression);
-    printf("find: unable to find due to lack of arguments");
-    exit(0);
-  }
-
-  for(i = 2; i < argc; i++){
-    if((fd = open(argv[i], O_RDONLY)) < 0){
-      printf("find: cannot open %s\n", argv[i]);
-      exit(1);
-    }
-    findit(path, expression);
-    close(fd);
-  }
-  */
   exit(0);
 }
 // Regexp matcher from Kernighan & Pike,
