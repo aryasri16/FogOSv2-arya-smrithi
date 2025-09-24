108ec56104b31ab1999fd8dc71dce0b00bec57a9
Smrithi Panuganti <spanuganti@dons.usfca.edu>
2025-09-24 12:37:40 -0700

    find has grep logic right now will change later

--- user/find.c
+++ user/find.c
@@ -1,12 +1,106 @@
 #include "kernel/types.h"
 #include "kernel/stat.h"
+#include "kernel/fcntl.h"
 #include "user/user.h"
 
+char buf[1024];
+int match(char*, char*);
+
+void
+find(char *path, char *expression)
+{
+  int n, m;
+  char *p, *q;
+
+  m = 0;
+  while((n = read(fd, buf+m, sizeof(buf)-m-1)) > 0){
+    m += n;
+    buf[m] = '\0';
+    p = buf;
+    while((q = strchr(p, '\n')) != 0){
+      *q = 0;
+      if(match(pattern, p)){
+        *q = '\n';
+        write(1, p, q+1 - p);
+      }
+      p = q+1;
+    }
+    if(m > 0){
+      m -= p - buf;
+      memmove(buf, p, m);
+    }
+  }
+}
 int
 main(int argc, char *argv[])
 {
-  printf("In the find.c file\n");
-  find("/home/user","document.pdf");
+  int fd, i;
+  char *path;
+  char *expression;
+
+  printf("in user space find.c file\n");
+
+  if(argc <= 1){
+    fprintf(2, "usage: find pattern [file ...]\n");
+    exit(1);
+  }
+  path = argv[1];
+  expression = argv[2];
+
+  if(argc <= 2){
+    find(path, );
+    fprintf("find: unable to find due to lack of arguments");
+    exit(0);
+  }
+
+  for(i = 2; i < argc; i++){
+    if((fd = open(argv[i], O_RDONLY)) < 0){
+      printf("find: cannot open %s\n", argv[i]);
+      exit(1);
+    }
+    find(pattern, fd);
+    close(fd);
+  }
+  exit(0);
+}
+// Regexp matcher from Kernighan & Pike,
+// The Practice of Programming, Chapter 9, or
+// https://www.cs.princeton.edu/courses/archive/spr09/cos333/beautiful.html
+
+int matchhere(char*, char*);
+int matchstar(int, char*, char*);
+
+int
+match(char *re, char *text)
+{
+  if(re[0] == '^')
+    return matchhere(re+1, text);
+  do{  // must look at empty string
+    if(matchhere(re, text))
+      return 1;
+  }while(*text++ != '\0');
   return 0;
-  
+}
+// matchhere: search for re at beginning of text
+int matchhere(char *re, char *text)
+{
+  if(re[0] == '\0')
+    return 1;
+  if(re[1] == '*')
+    return matchstar(re[0], re+2, text);
+  if(re[0] == '$' && re[1] == '\0')
+    return *text == '\0';
+  if(*text!='\0' && (re[0]=='.' || re[0]==*text))
+    return matchhere(re+1, text+1);
+  return 0;
+}
+
+// matchstar: search for c*re at beginning of text
+int matchstar(int c, char *re, char *text)
+{
+  do{  // a * matches zero or more instances
+    if(matchhere(re, text))
+      return 1;
+  }while(*text!='\0' && (*text++==c || c=='.'));
+  return 0;
 }
